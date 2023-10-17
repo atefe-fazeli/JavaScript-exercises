4fab9784d7018752593f82cbf27d756f0aff02ca
AtefeFazeli <AtefeFazeli.com>
2023-10-17 12:17:26 +0330

    write add and delete functions

--- explain-of-note-app-project/source/js/app.js
+++ explain-of-note-app-project/source/js/app.js
@@ -2,19 +2,39 @@ let $ = document;
 
 let inputElem = $.getElementById("input-field");
 let noteParent = $.getElementById("listed");
+let btnSave = $.getElementById("btn-save");
+let btnDelete = $.getElementById("btn-delete");
 
-let noteElem = $.createElement("p");
-let noteContainer = $.createElement("div");
+function add() {
+  let noteElem = $.createElement("p");
+  let noteContainer = $.createElement("div");
+  console.log(inputElem.value);
+  noteElem.innerHTML = inputElem.value;
+  noteElem.classList.add("card-text", "p-3");
+  noteContainer.classList.add("card", "rounded", "shadow-sm");
+  noteParent.appendChild(noteContainer);
+  noteContainer.appendChild(noteElem);
+  inputElem.value = "";
+  function deleteNoteHandler() {
+    noteContainer.remove();
+  }
+  noteContainer.addEventListener("click", deleteNoteHandler);
+}
 
 function inputChangeHandler(event) {
   if (event.keyCode === 13) {
-    console.log(inputElem.value);
-    noteElem.innerHTML = inputElem.value;
-    noteElem.classList.add("card-text", "p-3");
-    noteContainer.classList.add("card", "rounded", "shadow-sm");
-    noteParent.appendChild(noteContainer);
-    noteContainer.appendChild(noteElem);
+    add();
   }
 }
 
+function saveHandler() {
+  add();
+}
+
+function deleteHandler() {
+  inputElem.value = "";
+}
+
 inputElem.addEventListener("keyup", inputChangeHandler);
+btnDelete.addEventListener("click", deleteHandler);
+btnSave.addEventListener("click", saveHandler);
