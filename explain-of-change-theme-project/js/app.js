a59e6b5a5f36c9b4f378407a64c8efd2d7031a24
AtefeFazeli <AtefeFazeli.com>
2023-11-06 15:04:34 +0330

    theme project js finished

--- explain-of-change-theme-project/js/app.js
+++ explain-of-change-theme-project/js/app.js
@@ -1,5 +1,30 @@
 const switchElement = document.querySelector('.switch')
+const body=document.body
+let themeFlag=false
+let theme
 
 switchElement.addEventListener('click', function () {
-  // Hint: Add 'dark' class to body :))
+themeFlag=!themeFlag
+console.log(themeFlag)
+if(themeFlag){
+  theme=localStorage.setItem('theme','dark')
+ }else{
+   theme=localStorage.setItem('theme','light')
+ }
+ theme=localStorage.getItem('theme')
+ if(theme==='dark'){
+  body.setAttribute('class','dark')
+ }else{
+  body.removeAttribute('class')
+ }
 })
+
+window.addEventListener('load',function (){
+  theme=localStorage.getItem('theme')
+  if(theme==='dark'){
+   body.setAttribute('class','dark')
+  }else{
+   body.removeAttribute('class')
+  }
+})
+
