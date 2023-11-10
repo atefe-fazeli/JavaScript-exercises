9ccbf4ea1378c6ad3a2cab1ef8637e996c8f8c31
AtefeFazeli <AtefeFazeli.com>
2023-11-10 18:52:24 +0330

    js code completed

--- explain-of-range-slider-project/js/script.js
+++ explain-of-range-slider-project/js/script.js
@@ -1,7 +1,12 @@
 let rangeElem=document.getElementById('range')
+let containerElem=document.getElementsByClassName('container')
+
 
 function rangeHandler(e){
     console.log(e.target.value)
+    containerElem[0].style.filter=`brightness(${e.target.value}%)`
+    
+    console.log(containerElem[0])
 }
 
 rangeElem.addEventListener('click',rangeHandler)
