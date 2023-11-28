1c75dd4071c0b1dc9f3a0cc5e72c311ae655a40b
AtefeFazeli <AtefeFazeli.com>
2023-11-28 13:18:08 +0330

    css and html completed

---
+++ width-height-project/js/app.js
@@ -0,0 +1,7 @@
+// DOM => Document Object Model
+// BOM => Browser Object Model
+
+let divElem = document.querySelector('div')
+
+console.log('clientWidth:', divElem.clientWidth);
+console.log('clientHeigth:', divElem.clientHeight);
